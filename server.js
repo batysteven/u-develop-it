669395df9526e54248e19d33aa74beabc4731917
Steven Baty <baty_steven@yahoo.com>
2021-02-28 02:12:25 -0700

    wrote query to get all candidates to server, works!

--- server.js
+++ server.js
@@ -15,9 +15,22 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
 
 // GET a single candidate
 // db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
@@ -36,21 +49,21 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this.lastID);
-});
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
-// Default response for any other request(Not Found) Catch all
-app.use((req, res) => {
-    res.status(404).end();
-});
+// // Default response for any other request(Not Found) Catch all
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
 
 // Start server after DB connection
 db.on('open', () => {
